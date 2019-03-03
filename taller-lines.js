97ab649c30165cae28f6acd9588126e71f5ae4be
moonypoo <47288881+moonypoo@users.noreply.github.com>
2019-03-02 23:28:17 -0800

    Create taller-lines.js

---
+++ taller-lines.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  for (var i = 10; i <= 390; i = i + 10) {
+   var x = height
+      line(i, 10, i, x-200);
+    }
+  
+}
